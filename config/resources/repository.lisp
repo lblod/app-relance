95518bb2ee334b9ad5647154a66786689f248ff7
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-05-19 14:52:34 +0200

    added nacebel

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -34,3 +34,4 @@
 (add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
 (add-prefix "schema" "http://schema.org/")
 (add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
+(add-prefix "nacebel" "http://data.gift/vocabularies/nace-bel/")
