bef3f55cff1009e22e68ee1c8bd343995f5c9bd3
Aad Versteden <madnificent@gmail.com>
2020-05-31 22:25:54 +0200

    New disptacher with fastboot frontend
    In this commit we've upgraded the dispatcher to a new format.  The
    sources now use a specific branch from the frontend which has support
    for fastboot.
    
    The new construction makes it easier to set up fastboot as a service.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,16 +1,16 @@
 defmodule Dispatcher do
-  use Plug.Router
+  use Matcher
 
-  def start(_argv) do
-    port = 80
-    IO.puts "Starting Plug with Cowboy on port #{port}"
-    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
-    :timer.sleep(:infinity)
-  end
+  define_accept_types [
+    json: [ "application/json", "application/vnd.api+json" ],
+    html: [ "text/html", "application/xhtml+html" ],
+    any: ["*/*"]
+  ]
 
-  plug Plug.Logger
-  plug :match
-  plug :dispatch
+  @json %{ accept: %{ json: true } }
+  @html %{ accept: %{ html: true } }
+  @any %{ accept: %{ any: true } }
+
 
   # In order to forward the 'themes' resource to the
   # resource service, use the following forward rule.
@@ -18,40 +18,58 @@ defmodule Dispatcher do
   # docker-compose stop; docker-compose rm; docker-compose up
   # after altering this file.
   #
-  # match "/themes/*path" do
+  # match "/themes/*path", _ do
   #   Proxy.forward conn, path, "http://resource/themes/"
   # end
+  #
+  # match "/themes/*path", { last_call: true } do
+  #   send_resp( conn, 404, "Route not found, check config/dispatcher.ex" )
+  # end
 
-  match "/local-businesses/*path" do
+  match "/favicon.ico", @any do
+    send_resp( conn, 404, "There is no favicon here" )
+  end
+
+  match "/assets/*path", @any do
+    Proxy.forward conn, path, "http://fastboot/assets/"
+  end
+
+  match "/*path", @html do
+    Proxy.forward conn, path, "http://fastboot/"
+  end
+
+  match "/local-businesses/*path", @json do
     Proxy.forward conn, path, "http://cache/local-businesses/"
   end
 
-  match "/locations/*path" do
+  match "/locations/*path", @json do
     Proxy.forward conn, path, "http://cache/locations/"
   end
 
-  match "/categories/*path" do
+  match "/categories/*path", @json do
     Proxy.forward conn, path, "http://cache/categories/"
   end
 
-  match "/opening-hours-specifications/*path" do
+  match "/opening-hours-specifications/*path", @json do
     Proxy.forward conn, path, "http://cache/opening-hours-specifications/"
   end
 
-  match "/day-of-weeks/*path" do
+  match "/day-of-weeks/*path", @json do
     Proxy.forward conn, path, "http://cache/day-of-weeks/"
   end
 
-  match "/nace-bel-codes/*path" do
+  match "/nace-bel-codes/*path", @json do
     Proxy.forward conn, path, "http://cache/nace-bel-codes/"
   end
 
-  get "/extract/*path" do
+  get "/extract/*path", @json do
     Proxy.forward conn, path, "http://extract-local-businesses-from-url/"
   end
 
-  match _ do
+  match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
+
+  last_match
 
 end
