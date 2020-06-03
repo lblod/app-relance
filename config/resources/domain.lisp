1b1851a8487ef44d57e0aca715aefe03f4862eec
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-03 13:02:46 +0200

    Enable pagination and cache headers in mu-cl-resources

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,5 +1,12 @@
 (in-package :mu-cl-resources)
 
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+
 ;;;;
 ;; NOTE
 ;; docker-compose stop; docker-compose rm; docker-compose up
